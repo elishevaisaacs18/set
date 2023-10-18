9cb51f72bff9086c4bf7df5d5927b5ceb1ab5a58
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-10-18 20:11:57 +0300

    href to welcome page

--- js/createNewAccount.js
+++ js/createNewAccount.js
@@ -1,6 +1,3 @@
-//! delete empty lines
-//!ctrl+shift+i
-
 function User(email, userName, password) {
     this.email = email;
     this.userName = userName;
@@ -13,9 +10,5 @@ function saveUser() {
     const email = document.getElementById('email').value;
     const userName = document.getElementById('uname').value;
     const password = document.getElementById('pass').value;
-    //! i think you should change the key name in local storage to user, since you are saving all the user as an object
     localStorage.setItem(userName, JSON.stringify(new User(email, userName, password)))
-
 }
-
-document.getElementById('sign-up-btn').addEventListener('click', saveUser)
