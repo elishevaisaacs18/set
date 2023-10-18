c6cb745cd3c1b03e5ee0d1105c37ad16e7b4fee6
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-10-18 21:23:56 +0300

    log in check

--- js/welcomePage.js
+++ js/welcomePage.js
@@ -1,3 +1,7 @@
+document.getElementById('link-to-game').addEventListener(onload, disableLink)
+function disableLink (event){
+    event.preventDefault();
+}
 document.getElementById('sign-in-btn').addEventListener('click', accessAccount)
 
 //! i would like to go throgh this function with you
@@ -5,16 +9,18 @@ function accessAccount() {
     const userNameInput = document.getElementById('uname').value;
     const passwordInput = document.getElementById('pass').value;
 
-    if (localStorage.getItem(userName) !== null) {
+    if (localStorage.getItem(userNameInput) === null) {
         document.getElementById('alert-mistakeOrNoaccount').textContent = 'your username is incorrect or you dont have have an account, creat one down below'
         document.getElementById('alert-mistakeOrNoaccount').classList[0].remove;
     }
     else {
-        let user = JSON.parse(localStorage.getItem(userName));
+        let user = JSON.parse(localStorage.getItem(userNameInput));
+        console.log(user)
         if (user.userName === userNameInput && user.password === passwordInput) {
-            var newlink = document.createElement('a');
-            newlink.setAttribute('href', './welcomePage.html')
-            document.getElementById('sign-in-btn').appendChild(newlink);
+            /*var newlink = document.createElement('a');
+            newlink.setAttribute('href', 'gamePage.html')
+            document.getElementById('sign-in-btn').appendChild(newlink);*/
+            document.getElementById('link-to-game').removeEventListener(onload, disableLink);
         }
         else {
             document.getElementById('alert-mistakeOrNoaccount').textContent = 'your password is incorrect'
@@ -22,3 +28,4 @@ function accessAccount() {
         }
     }
 }
+
