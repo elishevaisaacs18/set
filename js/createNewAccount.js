6b8f40d45140ef0a7723b5a5c5cd2c2d3db0b7fb
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-10-19 11:51:05 +0300

    validation sign up function

--- js/createNewAccount.js
+++ js/createNewAccount.js
@@ -12,3 +12,45 @@ function saveUser() {
     const password = document.getElementById('pass').value;
     localStorage.setItem(userName, JSON.stringify(new User(email, userName, password)))
 }
+
+const checkValidation = (elementId, regex, messageId) => {
+    const message = document.getElementById(messageId);
+    const inputElement = document.getElementById(elementId);
+
+    if (!regex.test(inputElement.value)) {
+        message.classList.add("show");
+    } else {
+        if (message.classList.contains('show')) {
+            message.classList.remove('show');
+        }
+    }
+}
+
+const comfirmPass = () => {
+    const message = document.getElementById("confirm-pass-error-check");
+    if(document.getElementById('pass').value !== 
+    document.getElementById('confirm-pass').value) {
+        message.classList.add("show");
+    } else {
+        if (message.classList.contains('show')) {
+            message.classList.remove('show');
+        }
+    }
+}
+
+document.getElementById("email").addEventListener('input', () => {
+    checkValidation("email", /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "email-error-check");
+});
+
+document.getElementById("uname").addEventListener('input', () => {
+    checkValidation("uname", /^[A-Za-z0-9]{4,}$/, "uname-error-check");
+});
+
+document.getElementById("pass").addEventListener('input', () => {
+    checkValidation("pass", /^(?=.*[A-Z])(?=.*\d).{8,}$/, "pass-error-check");
+});
+
+document.getElementById("confirm-pass").addEventListener('input', comfirmPass);
+
+
+
--- js/gamePage.js
+++ js/gamePage.js
@@ -101,7 +101,7 @@ for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
     if (key === keyToCheck && value === 'true') {
-        document.getElementById("user-greeting").textContent = `Hello ${localStorage.}`;
+        document.getElementById("user-greeting").textContent = `Hello ${localStorage}`;
     }
 }
 
